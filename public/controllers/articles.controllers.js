82b6888ab7ec4a12f5856836ebfbf21b1ecdff94
Stefan <stefantrajkovic777@gmail.com>
2016-11-28 01:09:23 +0100

    controllers added, views created, pulling data...

---
+++ public/controllers/articles.controllers.js
@@ -0,0 +1,18 @@
+angular.module("ngFrenzy")
+
+.controller('ArticlesCtrl', ['$scope','$http', function($scope, $http) {
+  $http.get('/articles').success(function(data) {
+    $scope.articles = data;
+  });
+}])
+.controller('ArticlesCategoryCtrl', ['$scope','$http', '$routeParams', function($scope, $http, $routeParams) {
+  $http.get('/articles/category/'+$routeParams.category).success(function(data) {
+    $scope.cat_articles = data;
+    $scope.category = $routeParams.category;
+  });
+}])
+.controller('ArticleDetailsCtrl', ['$scope','$http', '$routeParams', function($scope, $http, $routeParams) {
+  $http.get('/articles/'+$routeParams.id).success(function(data) {
+    $scope.article = data;
+  });
+}]);
